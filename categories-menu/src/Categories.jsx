d34e9e004982275188dc7519f652dd01391c693a
Uzair Asif <44237563+uzair-asif@users.noreply.github.com>
2021-08-08 11:24:17 +0500

    dynamic-categories-menu-project

---
+++ categories-menu/src/Categories.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className='btn-container'>
+      {categories.map((category, index) => {
+        return (
+          <button
+            type='button'
+            key={index}
+            onClick={() => {
+              filterItems(category);
+            }}
+            className='filter-btn'
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
